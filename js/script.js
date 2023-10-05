9b150a0bddcacd8149026c663bf64f3288a95627
Delano <del.191006@gmail.com>
2023-10-05 14:13:13 +0700

    implementing smooth scrolling to all browser

--- js/script.js
+++ js/script.js
@@ -6,3 +6,27 @@ const navbar = document.querySelector(".navigation-bar");
 btnNavEl.addEventListener("click", function () {
   navbar.classList.toggle("nav-open");
 });
+
+//SMOOTH SCROLLING ANIMATION
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    //scroll ke atas
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
