04a332239491bbe49d07a8b7a323ba2885bb9052
Delano <del.191006@gmail.com>
2023-10-05 14:16:23 +0700

    implementing smooth scrolling to all browser and mobile device

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    console.log(href);
 
     //scroll ke atas
     if (href === "#") {
@@ -27,6 +26,11 @@ allLinks.forEach(function (link) {
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // tutup navigasi mobile
+    if (link.classList.contains("main-nav-link")) {
+      navbar.classList.toggle("nav-open");
     }
   });
 });
